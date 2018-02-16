3a9d4c5868d8bb31f880af843ffc5db51f101d16
kawa <akimaleo@ya.ru>
2018-02-16 22:02:35 +0200

    Initial commit

---
+++ macStatus/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  macStatus
+//
+//  Created by Yaroslav Movchan on 16.02.2018.
+//  Copyright © 2018 Yaroslav Movchan. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
